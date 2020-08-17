4372e08de91710e86b38239bbec2f4c7c7818335
Nadja Reischel <nadja.reischel@outlook.de>
2020-08-17 11:28:05 +0200

    done

--- src/movies.js
+++ src/movies.js
@@ -1,12 +1,25 @@
 // Iteration 1: All directors? - Get the array of all directors.
+    
+    function getAllDirectors(movies) {
+        return movies.map (function (movie) {
+            return movie.director
+        })
+    }
 
-
-    const getAllDirectors = movies.map(function(movie) {
-        return movie.director
-    })
-
+    
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+
+    function getAllDirectors(movies) {
+        return directors = movies.map (function (movie) {
+            return movie.director
+        })
+        director.filter(function (director, index, directors) {
+            return directors.indexOf(director) === index
+        })
+    }
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
@@ -57,11 +70,30 @@ function dramaMovieRate(movies) {
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
 function orderByYear(movies) {
-    
+    const sorted = movies.slice().sort(function (a,b){
+        if (a.year !== b.year) {
+            return a.year - b.year;
+        } else {
+            return a.title.localeCompare(b.title)
+        }
+    });
+    return sorted;
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
+    function orderAlphabetically(movies) {
+        const ordered = movies.map(function (movie){
+            return movie.title
+        }).sort(function (a,b){
+            return a.localeCompare (b);
+        }).slice(0,20);
+
+        return ordered
+    }
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+
+
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
