cd73ead1dc21918bd54e9a9ca1ae38770e6a2e64
Nadja Reischel <nadja.reischel@outlook.de>
2020-08-17 09:07:06 +0200

    done

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,64 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+
+    const getAllDirectors = movies.map(function(movie) {
+        return movie.director
+    })
+
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+function howManyMovies(movies) {
+    const listOfSpielbergDramas = movies.filter(function (movie) {
+        if (movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')) {
+          return true
+        }
+      })
+      return listOfSpielbergDramas.length;
+}
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
+function ratesAverage(movies) {
+    if (movies.length === 0) {
+      return 0} 
+      
+      const filmsWithRating = movies.filter(function (movie) {
+      if (movie.rate) { 
+        return true }
+      })
+      
+      const averageRate = filmsWithRating.reduce(function(acc, val) {
+      return (acc + val.rate)}, 0)
+      return Number ((averageRate/movies.length).toFixed(2))
+     
+    }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMovieRate(movies) {
+    if (movies.length === 0) {
+        return 0} 
+    
+        const filmsWithRating = movies.filter(function (movie) {
+            if (movie.genre.includes('Drama')) { 
+              return true }
+            })
+
+        const averageRate = filmsWithRating.reduce(function(acc, val) {
+            return (acc + val.rate)}, 0)
+            return Number ((averageRate/movies.length).toFixed(2))
+               
+
+}
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+function orderByYear(movies) {
+    
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
